dfc35375fa6dd861c84fbc9ddc283170c28cd165
Joe Evert <joeevert@gmail.com>
2019-02-01 10:21:20 -0600

    fixed the svg element positioning - added some mui styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,27 +4,12 @@ import './App.css';
 import Nav from '../Nav/Nav';
 import Mushroom from '../Mushroom/Mushroom';
 
-const colors =['red', 'blue', 'green', 'yellow'];
-
 class App extends Component {
-  state= {
-    bgColor: 'green'
-  };
-
-  selectColor = (color) => {
-    this.setState({ bgColor: color });
-  }
 
   render() {
     return (
       <div className="App">
-        {/* <Nav /> */}
-        {/* <div className={this.state.bgColor} /><br /> */}
-        {/* <button onClick={this.changeColor} style={{margin: 0, padding: 0}}>
-          Change color
-        </button> */}
-        {/* {colors.map( (color, index) =>
-        <div key={index} className={color} onClick={() => this.selectColor(color)}/>)} */}
+        <Nav />
         <Mushroom />
       </div>
     );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './Nav.css';
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 
 class Nav extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <header className="Nav-header">
-          <img src={logo} className="Nav-logo" alt="logo" />
-          <p>
-            Edit <code>src/Nav.js</code> and save to reload.
-          </p>
-          <a
-            className="Nav-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.root}>
+        <AppBar className={classes.appBar} position="static">
+          <Typography className={classes.header} variant="h3">MUSHROOM MASHUP</Typography>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Nav;
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    backgroundColor: 'hsl(0, 100%, 50%)',
+  },
+  header: {
+    color: '#fff',
+    margin: '.5em'
+  }
+}
+
+export default withStyles(styles)(Nav);
